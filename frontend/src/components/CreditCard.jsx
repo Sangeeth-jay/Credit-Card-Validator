76fd279af8517c830b77e9853a5f3b5bf035283b
Sangeeth-jay <sangeethj74@gmail.com>
2024-05-16 20:12:09 +0530

    create image change function

--- frontend/src/components/CreditCard.jsx
+++ frontend/src/components/CreditCard.jsx
@@ -1,54 +1,151 @@
 /* eslint-disable react/prop-types */
 const CreditCard = (props) => {
+  const { name, number, expiry, cvv } = props;
 
-const {name, number, expiry, cvv} = props
+  const ccLogo = [
+    {
+      name: "Visa",
+      img: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="48"
+          height="48"
+          viewBox="0 0 48 48"
+        >
+          <path
+            fill="#1565C0"
+            d="M45,35c0,2.209-1.791,4-4,4H7c-2.209,0-4-1.791-4-4V13c0-2.209,1.791-4,4-4h34c2.209,0,4,1.791,4,4V35z"
+          ></path>
+          <path
+            fill="#FFF"
+            d="M15.186 19l-2.626 7.832c0 0-.667-3.313-.733-3.729-1.495-3.411-3.701-3.221-3.701-3.221L10.726 30v-.002h3.161L18.258 19H15.186zM17.689 30L20.56 30 22.296 19 19.389 19zM38.008 19h-3.021l-4.71 11h2.852l.588-1.571h3.596L37.619 30h2.613L38.008 19zM34.513 26.328l1.563-4.157.818 4.157H34.513zM26.369 22.206c0-.606.498-1.057 1.926-1.057.928 0 1.991.674 1.991.674l.466-2.309c0 0-1.358-.515-2.691-.515-3.019 0-4.576 1.444-4.576 3.272 0 3.306 3.979 2.853 3.979 4.551 0 .291-.231.964-1.888.964-1.662 0-2.759-.609-2.759-.609l-.495 2.216c0 0 1.063.606 3.117.606 2.059 0 4.915-1.54 4.915-3.752C30.354 23.586 26.369 23.394 26.369 22.206z"
+          ></path>
+          <path
+            fill="#FFC107"
+            d="M12.212,24.945l-0.966-4.748c0,0-0.437-1.029-1.573-1.029c-1.136,0-4.44,0-4.44,0S10.894,20.84,12.212,24.945z"
+          ></path>
+        </svg>
+      ),
+    },
+    {
+      name: "Mastercard",
+      img: (
+        <svg
+          className="text-white fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          viewBox="0 0 48 48"
+        >
+          <path
+            fill="#ff9800"
+            d="M32 10A14 14 0 1 0 32 38A14 14 0 1 0 32 10Z"
+          ></path>
+          <path
+            fill="#d50000"
+            d="M16 10A14 14 0 1 0 16 38A14 14 0 1 0 16 10Z"
+          ></path>
+          <path
+            fill="#ff3d00"
+            d="M18,24c0,4.755,2.376,8.95,6,11.48c3.624-2.53,6-6.725,6-11.48s-2.376-8.95-6-11.48 C20.376,15.05,18,19.245,18,24z"
+          ></path>
+        </svg>
+      ),
+    },
+    {
+      name: "Amex",
+      img: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="48"
+          height="48"
+          viewBox="0 0 48 48"
+        >
+          <path
+            fill="#1976D2"
+            d="M45,35c0,2.209-1.791,4-4,4H7c-2.209,0-4-1.791-4-4V13c0-2.209,1.791-4,4-4h34c2.209,0,4,1.791,4,4V35z"
+          ></path>
+          <path
+            fill="#FFF"
+            d="M22.255 20l-2.113 4.683L18.039 20h-2.695v6.726L12.341 20h-2.274L7 26.981h1.815l.671-1.558h3.432l.682 1.558h3.465v-5.185l2.299 5.185h1.563l2.351-5.095v5.095H25V20H22.255zM10.135 23.915l1.026-2.44 1.066 2.44H10.135zM37.883 23.413L41 20.018h-2.217l-1.994 2.164L34.86 20H28v6.982h6.635l2.092-2.311L38.767 27h2.21L37.883 23.413zM33.728 25.516h-4.011v-1.381h3.838v-1.323h-3.838v-1.308l4.234.012 1.693 1.897L33.728 25.516z"
+          ></path>
+        </svg>
+      ),
+    },
+    {
+      name: "Discover",
+      img: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="48"
+          height="48"
+          viewBox="0 0 48 48"
+        >
+          <path
+            fill="#E1E7EA"
+            d="M45,35c0,2.2-1.8,4-4,4H7c-2.2,0-4-1.8-4-4V13c0-2.2,1.8-4,4-4h34c2.2,0,4,1.8,4,4V35z"
+          ></path>
+          <path
+            fill="#FF6D00"
+            d="M45,35c0,2.2-1.8,4-4,4H16c0,0,23.6-3.8,29-15V35z M22,24c0,1.7,1.3,3,3,3s3-1.3,3-3c0-1.7-1.3-3-3-3S22,22.3,22,24z"
+          ></path>
+          <path d="M11.2,21h1.1v6h-1.1V21z M17.2,24c0,1.7,1.3,3,3,3c0.5,0,0.9-0.1,1.4-0.3v-1.3c-0.4,0.4-0.8,0.6-1.4,0.6c-1.1,0-1.9-0.8-1.9-2c0-1.1,0.8-2,1.9-2c0.5,0,0.9,0.2,1.4,0.6v-1.3c-0.5-0.2-0.9-0.4-1.4-0.4C18.5,21,17.2,22.4,17.2,24z M30.6,24.9L29,21h-1.2l2.5,6h0.6l2.5-6h-1.2L30.6,24.9z M33.9,27h3.2v-1H35v-1.6h2v-1h-2V22h2.1v-1h-3.2V27z M41.5,22.8c0-1.1-0.7-1.8-2-1.8h-1.7v6h1.1v-2.4h0.1l1.6,2.4H42l-1.8-2.5C41,24.3,41.5,23.7,41.5,22.8z M39.2,23.8h-0.3v-1.8h0.3c0.7,0,1.1,0.3,1.1,0.9C40.3,23.4,40,23.8,39.2,23.8z M7.7,21H6v6h1.6c2.5,0,3.1-2.1,3.1-3C10.8,22.2,9.5,21,7.7,21z M7.4,26H7.1v-4h0.4c1.5,0,2.1,1,2.1,2C9.6,24.4,9.5,26,7.4,26z M15.3,23.3c-0.7-0.3-0.9-0.4-0.9-0.7c0-0.4,0.4-0.6,0.8-0.6c0.3,0,0.6,0.1,0.9,0.5l0.6-0.8C16.2,21.2,15.7,21,15,21c-1,0-1.8,0.7-1.8,1.7c0,0.8,0.4,1.2,1.4,1.6c0.6,0.2,1.1,0.4,1.1,0.9c0,0.5-0.4,0.8-0.9,0.8c-0.5,0-1-0.3-1.2-0.8l-0.7,0.7c0.5,0.8,1.1,1.1,2,1.1c1.2,0,2-0.8,2-1.9C16.9,24.2,16.5,23.8,15.3,23.3z"></path>
+        </svg>
+      ),
+    },
+  ];
+
+  const catchFirst2Digit = () => {
+    const first2Digit = number.slice(0, 1);
+    return first2Digit;
+  }
+
+  const iconChange = () => {
+    if (catchFirst2Digit() === "4") {
+      return ccLogo[0].img;
+    } else if (catchFirst2Digit() === "5") {
+      return ccLogo[1].img;
+    } else if (catchFirst2Digit() === "3") {
+      return ccLogo[2].img;
+    } else if (catchFirst2Digit() === "6") {
+      return ccLogo[3].img;
+    }
+
+  }
 
   return (
     <>
-        <div className="flex flex-col justify-around bg-gradient-to-tl from-gray-700 via-20% to-gray-800 to-90% p-4 border border-white border-opacity-30 rounded-lg shadow-md w-80 h-52">
-          <div className="flex flex-row items-center justify-between mb-3">
-            <label className="text-sm text-slate-300 font-semibold pl-2 mb-3 flex-grow">
-              {name}
-            </label>
-            <div className="pr-2">
-              <div className="flex items-center justify-center relative w-14 h-9 bg-gray-800 border border-white border-opacity-20 rounded-md">
-                <svg
-                  className="text-white fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 48 48"
-                >
-                  <path
-                    fill="#ff9800"
-                    d="M32 10A14 14 0 1 0 32 38A14 14 0 1 0 32 10Z"
-                  ></path>
-                  <path
-                    fill="#d50000"
-                    d="M16 10A14 14 0 1 0 16 38A14 14 0 1 0 16 10Z"
-                  ></path>
-                  <path
-                    fill="#ff3d00"
-                    d="M18,24c0,4.755,2.376,8.95,6,11.48c3.624-2.53,6-6.725,6-11.48s-2.376-8.95-6-11.48 C20.376,15.05,18,19.245,18,24z"
-                  ></path>
-                </svg>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col space-y-3">
-            <label className="text-xl text-slate-300 font-semibol pl-2">
-              {number}
-            </label>
-            <div className="flex flex-row justify-between">
-              <label className=" text-sm text-slate-300 font-semibold pl-2">
-                {expiry}
-              </label>
-              <label className=" text-sm text-slate-300 font-semibold pr-2">
-                {cvv}
-              </label>
+      <div className="flex flex-col justify-around bg-gradient-to-tl from-gray-700 via-20% to-gray-800 to-90% p-4 border border-white border-opacity-30 rounded-lg shadow-md w-80 h-52">
+        <div className="flex flex-row items-center justify-between mb-3">
+          <label className="text-sm text-slate-300 font-semibold pl-2 mb-3 flex-grow">
+            {name}
+          </label>
+          <div className="pr-2">
+            <div className="flex items-center justify-center relative w-12 h-9 bg-gray-800 border border-white border-opacity-20 rounded-md">
+              {iconChange()}
             </div>
           </div>
         </div>
+        <div className="flex flex-col space-y-3">
+          <label className="text-xl text-slate-300 font-semibol pl-2">
+            {number}
+          </label>
+          <div className="flex flex-row justify-between">
+            <label className=" text-sm text-slate-300 font-semibold pl-2">
+              {expiry}
+            </label>
+            <label className=" text-sm text-slate-300 font-semibold pr-2">
+              {cvv}
+            </label>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
