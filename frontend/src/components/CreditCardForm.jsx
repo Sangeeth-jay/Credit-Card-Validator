d38f0da8ccea8256ce72de171f1d81e0bb0f3569
Sangeeth-jay <sangeethj74@gmail.com>
2024-05-19 10:12:23 +0530

    fix the bugs

--- frontend/src/components/CreditCardForm.jsx
+++ frontend/src/components/CreditCardForm.jsx
@@ -14,7 +14,6 @@ const CreditCardForm = () => {
 
   const handleChange = (event) => {
     setCardDetails({ ...cardDetails, [event.target.name]: event.target.value });
-    console.log(cardDetails);
   };
 
   const luhnMethod = (ccNumber) => {
@@ -61,8 +60,7 @@ const CreditCardForm = () => {
                 onChange={handleChange}
                 type="text"
                 name="name"
-                id=""
-                placeholder="ex : A. V. Suraveera"
+                placeholder="ex: A. V. Suraveera"
                 className="w-80 bg-white border-2 border-slate-400 outline-none focus:border-blue-500 rounded-md px-2 py-2 text-lg "
               />
             </div>
@@ -72,7 +70,6 @@ const CreditCardForm = () => {
                 onChange={handleChange}
                 type="text"
                 name="number"
-                id=""
                 placeholder="xxxx xxxx xxxx xxxx"
                 className="w-80 bg-white border-2 border-slate-400 outline-none focus:border-blue-500 rounded-md px-2 py-2 text-lg "
               />
@@ -84,7 +81,6 @@ const CreditCardForm = () => {
                   onChange={handleChange}
                   type="text"
                   name="expiry"
-                  id=""
                   placeholder="mm/yy"
                   className="w-20 bg-white border-2 border-slate-400 outline-none focus:border-blue-500 rounded-md px-2 py-2 text-lg "
                 />
@@ -95,7 +91,6 @@ const CreditCardForm = () => {
                   onChange={handleChange}
                   type="text"
                   name="cvv"
-                  id=""
                   placeholder="XXX"
                   className="w-20 bg-white border-2 border-slate-400 outline-none focus:border-blue-500 rounded-md px-2 py-2 text-lg "
                 />
@@ -104,23 +99,14 @@ const CreditCardForm = () => {
           </div>
           <div className="w-full flex justify-center items-center mt-4">
             <button
-              onClick={() => isValidCC()}
+              onClick={isValidCC}
               className="w-40 h-10 bg-green-600 text-white rounded-md hover:bg-green-700 duration-200 hover:font-semibold active:scale-95"
             >
               Check Validity
             </button>
           </div>
           <div className="mt-4">
-            {isValid !== null && (
-              <h1
-                className={`bg-${
-                  isValid ? "green" : "red"
-                }-400 text-white rounded-md text-center py-2 `}
-              >
-                Your Card is{" "}
-                {isValid ? "Valid" : "Invalid. Please Check Again"}
-              </h1>
-            )}
+            {isValid !== null && (isValid ? <p className="text-green-500 text-lg border-2 border-green-400 px-4 py-2 text-center rounded-lg">Your Card is Valid</p> : <p className="text-red-500 text-lg border-2 border-red-400 px-4 py-2 text-center rounded-lg">Your Card is Invalid. Please Check Again</p>)}
           </div>
         </div>
         <div className="relative bottom-1/4">
